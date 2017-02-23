94b0d34d9c5491dd4a6de4448de46c982c01beae
Aijun Bai <aijunbai@gmail.com>
2017-02-23 14:17:31 -0800

    fixed a compiling error

--- src/HierarchicalFSMAgent.h
+++ src/HierarchicalFSMAgent.h
@@ -8,11 +8,6 @@
 #include "HierarchicalAgent.h"
 #include "dot_graph.h"
 
-#include <boost/archive/xml_iarchive.hpp>
-#include <boost/archive/xml_oarchive.hpp>
-#include <boost/serialization/vector.hpp>
-#include <boost/serialization/unordered_map.hpp>
-#include <boost/serialization/string.hpp>
 #include <boost/algorithm/string/replace.hpp>
 
 using namespace std;
